aa138357d1c0d4500524b021511758b3f881068f
automaticdai <automatic.dai@gmail.com>
2021-03-28 10:39:27 +0100

    add some comments in the script

--- src/main.py
+++ src/main.py
@@ -6,15 +6,23 @@ from time import sleep
 
 
 camera = PiCamera()
+
+# set the desired resoultion
 camera.resolution = (1280, 720)
 
-delay_cnt = 3 * 60 * 12; 
-shooting_cnt = 8 * 60 * 12;
+# delay before start (in seconds)
+delay_s = 3; 
+
+# interval between images (in seconds)
+interval_s = 5;
+
+# overall number of photos to take
+shooting_cnt = 8 * 60 * int(60 / interval_s);
 
 while(delay_cnt):
     print('Delay cnd: %d' % delay_cnt);
     delay_cnt = delay_cnt - 1;
-    sleep(5)
+    sleep(1)
 
 
 while(shooting_cnt):
@@ -22,4 +30,4 @@ while(shooting_cnt):
     camera.capture('/home/pi/timelapse/' + filename_str + '.jpg')
     shooting_cnt = shooting_cnt - 1;
     print('Cnt: %d' % shooting_cnt);
-    sleep(5)
+    sleep(interval_s)
